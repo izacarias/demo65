aa1daae48a8c1bc88d35587f19b802b834a885c0
izacarias <i.zacarias@gmail.com>
2023-09-19 19:09:30 +0200

    Updating the ONOS synch to SPARQL

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
 import logging
-import time
 from time import sleep
 from requests.auth import HTTPBasicAuth
 from onos import Link
-
+from pprint import pprint
 from rdflib import Namespace, XSD, Graph, Literal
-from rdflib.namespace import RDF
+from rdflib.namespace import RDF, RDFS, OWL
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 from rdflib.graph import DATASET_DEFAULT_GRAPH_ID as default
 
@@ -22,87 +21,132 @@ FUSEKI_URL = "http://localhost:3030"
 FUSEKI_DATASET = "ONOS"
 FUSEKI_UPDATE_URL = f"{FUSEKI_URL}/{FUSEKI_DATASET}/update"
 FUSEKI_QUERY_URL = f"{FUSEKI_URL}/{FUSEKI_DATASET}/query"
+LINKS_DICTIONARY = {
+    'path1': 
+        {
+            'src_device': 'of:0000000000000001', 
+            'src_port': '2', 
+            'dst_device': 'of:0000000000000003', 
+            'dst_port': '2', 
+            'max_cap': 50000000
+        },
+    'path2': 
+        {
+            'src_device': 'of:0000000000000001', 
+            'src_port': '3', 
+            'dst_device': 
+            'of:0000000000000002', 
+            'dst_port': '1', 
+            'max_cap': 100000000
+        },
+}
+
+def create_ontology(graph: Graph, ns: Namespace):
+    """
+    Creates the RDF Structure and send ontology in the SPARQL Server
+    """
+    graph.add((ns.DataRate, RDF.type, ns.Measurement))
+    graph.add((ns.DataRateBWMeasurement, RDF.type, ns.BWMeasurement))
+    graph.add((ns.hasBWMeasurement, RDF.type, OWL.ObjectProperty))
+    graph.add((ns.hasBWMeasurement, RDFS.domain, ns.Measurement))
+    graph.add((ns.hasBWMeasurement, RDFS.range, ns.BWMeasurement))
+    graph.add((ns.Usage5Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage10Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage15Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage20Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage25Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage30Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage35Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage40Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage45Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage50Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage55Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage60Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage65Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage70Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage75Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage80Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage85Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage90Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage95Percent, RDF.type, ns.BWMeasurement))
+    graph.add((ns.Usage100Percent, RDF.type, ns.BWMeasurement))
+    logging.info("Ontology sent to SPARQL Server. Graph has a lenght of %s", len(graph))
 
 
-def is_duplicated(list, new_link:Link) -> bool:
+def update_sparqldb(link: Link, graph: Graph, ns: Namespace):
     """
-    This function checks for duplicated links or
-    when the same link is reported in the opposite
-    direction by ONOS REST API.
+    Publishes the link statistics (data rate) to Apache Jena/Fuseki
+    SPARQL database.
     """
-    for link in list:
-        if (link.src_port == new_link.src_port and 
-            link.src_device == new_link.src_device and
-            link.dst_port == new_link.dst_port and
-            link.dst_device == new_link.dst_device
-            ) or ( link.src_port == new_link.dst_port and 
-            link.src_device == new_link.dst_device and
-            link.dst_port == new_link.src_port and
-            link.dst_device == new_link.src_device ):
-            return True
-    return False
-
-
-def get_links():
-    """
-    Get all links from ONOS REST API
-    """
-    return_value = []
-    response = requests.get(ONOS_LINK_URL, auth=HTTPBasicAuth(ONOS_USER, ONOS_PASS))
-    if response.status_code == 200:
-        id = 1;
-        for l in response.json()['links']:
-            new_link = Link(id, l['src']['port'], l['src']['device'], l['dst']['port'], l['dst']['device'])
-            if not is_duplicated(return_value, new_link):
-                return_value.append(new_link)
-                id  = id + 1
+    usage = link.get_link_usage()
+    usageObject = None
+    if usage <= 0.05:
+        usageObject = ns.Usage5Percent
+    elif usage <= 0.10:
+        usageObject = ns.Usage10Percent
+    elif usage <= 0.15:
+        usageObject = ns.Usage15Percent
+    elif usage <= 0.20:
+        usageObject = ns.Usage20Percent
+    elif usage <= 0.25:
+        usageObject = ns.Usage25Percent
+    elif usage <= 0.30:
+        usageObject = ns.Usage30Percent
+    elif usage <= 0.35:
+        usageObject = ns.Usage35Percent
+    elif usage <= 0.40:
+        usageObject = ns.Usage40Percent
+    elif usage <= 0.45:
+        usageObject = ns.Usage45Percent
+    elif usage <= 0.50:
+        usageObject = ns.Usage50Percent
+    elif usage <= 0.55:
+        usageObject = ns.Usage55Percent
+    elif usage <= 0.60:
+        usageObject = ns.Usage60Percent
+    elif usage <= 0.65:
+        usageObject = ns.Usage65Percent
+    elif usage <= 0.70:
+        usageObject = ns.Usage70Percent
+    elif usage <= 0.75:
+        usageObject = ns.Usage75Percent
+    elif usage <= 0.80:
+        usageObject = ns.Usage80Percent
+    elif usage <= 0.85:
+        usageObject = ns.Usage85Percent
+    elif usage <= 0.90:
+        usageObject = ns.Usage90Percent
+    elif usage <= 0.95:
+        usageObject = ns.Usage95Percent
+    elif usage > 0.95:
+        usageObject = ns.Usage100Percent
     else:
-        logging.error("Error getting list of links: %s", response.text)
-    logging.debug("Collected %s links from the topology", len(return_value))
-    return return_value
+        logging.error('Error creating the link usage object')
+    # remove old values
+    graph.remove((ns.MyMeasurement, ns.hasBWMeasurement, None))
+    graph.remove((ns.MyMeasurement, RDF.type, ns.BWMeasurement))
+    # add new values 
+    graph.add((ns.MyMeasurement, RDF.type, ns.BWMeasurement))
+    graph.add((ns.MyMeasurement, ns.hasBWMeasurement, usageObject))
+    logging.info("Graph sent to SPARQL server with lenght %s", len(graph))
 
-
-def get_link_stats(l: Link) -> int:
+def get_link_stats(link: Link) -> int:
     """ 
     This function gets the delta of bytesSent and bytesReceived as well as 
     the time passed since last readings. Based in that we can calculate
     the link rate in that moment.
     """
-
-    url = f"{ONOS_PORT_STAT_URL}/{l.src_device}/{l.src_port}"
+    url = f"{ONOS_PORT_STAT_URL}/{link.src_device}/{link.src_port}"
     response = requests.get(url, auth=HTTPBasicAuth(ONOS_USER, ONOS_PASS))
     if response.status_code == 200:
         bytesSent = int(response.json()['statistics'][0]['ports'][0]['bytesSent'])
         bytesReceived = int(response.json()['statistics'][0]['ports'][0]['bytesReceived'])
         duration = int(response.json()['statistics'][0]['ports'][0]['durationSec'])
-        l.set_data_rate(bytesSent, bytesReceived, duration)
-        logging.debug("Link (%s, %s): Bytes sent %s, Bytes received: %s, Duration: %s, Datarate: %s", l.src_device, l.dst_device, bytesSent, bytesReceived, duration, l.rate)
+        link.set_data_rate(bytesSent, bytesReceived, duration)
+        logging.debug("Link (%s, %s): Bytes sent %s, Bytes received: %s, Duration: %s, Datarate: %s, Usage: %.2f", 
+                      link.src_device, link.dst_device, bytesSent, bytesReceived, duration, link.rate, link.get_link_usage() * 100)
     else:
         logging.error("Error getting link statistics: %s", response.text)
-
-
-def publish_rdf(timeStamp: int, link: Link, graph: Graph):
-    """
-    Publishes the link statistics (data rate) to Apache Jena/Fuseki
-    SPARQL database.
-    """
-
-    logging.debug("Sending information to SPARQL: Link (%s, %s): %s bps", link.src_device, link.dst_device, link.get_data_rate())
-    ex = Namespace("http://6g-ric.de#")
-    # Link
-    graph.add((ex.a, RDF.type, ex.Link))
-    graph.add((ex.a, ex.hasOrigin, Literal(link.src_device)))
-    graph.add((ex.a, ex.hasDestination, Literal(link.dst_device)))
-    graph.add((ex.a, ex.hasDataRate, Literal(link.get_data_rate(), datatype=XSD.integer)))
-    # Timestamp
-    graph.add((ex.t, RDF.type, ex.Timestamp))
-    graph.add((ex.t, ex.hasTime, Literal(timeStamp, datatype=XSD.integer)))
-    # Measurement
-    graph.add((ex.measurement, RDF.type, ex.Measurement))
-    graph.add((ex.measurement, ex.MeasuresLink, ex.a))
-    graph.add((ex.measurement, ex.hasTimeStamp, ex.t))
-    logging.info("Graph sent to SPARQL server with lenght %s", len(graph))
-
 
 def main():
     """
@@ -110,24 +154,39 @@ def main():
     getting statistics about links and send them to the SPARQL
     database.
     """
+    # Prepare the ontology in SPARQL
+    store = SPARQLUpdateStore()
+    store.open((FUSEKI_QUERY_URL, FUSEKI_UPDATE_URL))
+    ns = Namespace("http://6g-ric.de#")
+    graph = Graph(store, identifier=default)
+    create_ontology(graph, ns)
+    links = []
+    for p in LINKS_DICTIONARY:
+        links.append(Link(
+            p,
+            LINKS_DICTIONARY[p]["src_device"],
+            LINKS_DICTIONARY[p]["src_port"],
+            LINKS_DICTIONARY[p]["dst_device"],
+            LINKS_DICTIONARY[p]["dst_port"],
+            LINKS_DICTIONARY[p]["max_cap"]
+        ))
     try:
-        links = []
-        while not len(links) > 0:
-            links = get_links()
-            sleep(READ_INTERVAL)
-        logging.debug("Detected %s links in the topology. Starting polling link stats.", len(links))
-        store = SPARQLUpdateStore()
-        store.open((FUSEKI_QUERY_URL, FUSEKI_UPDATE_URL))
-        graph = Graph(store, identifier=default)
         while True:
-            currentTime =  round(time.time())
+            # Get link stats
             for l in links:
-                # get stats 
                 get_link_stats(l)
-                publish_rdf(currentTime, l, graph)
+            
+            # Get the link with highest utilization
+            max_usage = 0.0
+            link_to_update = None
+            for l in links:
+                if l.get_link_usage() > max_usage:
+                    max_usage = l.get_link_usage()
+                    link_to_update = l
+            update_sparqldb(link=link_to_update, graph=graph, ns=ns)
             sleep(READ_INTERVAL)
+
     except KeyboardInterrupt:
-        graph.close()
         print("Script finished by CTRL+C command")
 
 if __name__ == '__main__':
--- onos.py
+++ onos.py
@@ -2,12 +2,13 @@
 
 class Link:
     
-    def __init__(self, id, src_port, src_device, dst_port, dst_device):
+    def __init__(self, id, src_device, src_port, dst_device, dst_port, max_capacity):
         self.id = id
         self.src_port = src_port
         self.src_device = src_device
         self.dst_port = dst_port
         self.dst_device = dst_device
+        self.max_capacity = max_capacity
         self.rate = 0
         self.lastBytesSent = 0
         self.lastBytesReceived = 0
@@ -16,13 +17,19 @@ class Link:
 
     def set_data_rate(self, bytesSent, bytesReceived, duration):
         if bytesSent >= bytesReceived:
-            self.rate = round((bytesSent - self.lastBytesSent) / (duration - self.lastDuration) * 8)
+            try:
+                self.rate = round((bytesSent - self.lastBytesSent) / (duration - self.lastDuration) * 8)
+            except ZeroDivisionError:
+                self.rate = 0
         else:
             self.rate = round((bytesReceived - self.lastBytesReceived) / (duration - self.lastDuration) * 8)
         self.lastBytesSent = bytesSent
         self.lastBytesReceived = bytesReceived
         self.lastDuration = duration
 
+    def get_link_usage(self):
+        return self.rate / self.max_capacity
 
     def get_data_rate(self) -> int:
         return self.rate
+
