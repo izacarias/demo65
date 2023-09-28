4f5d4f9b1968fcc2b675ca356f0c6e651b85f38a
izacarias <i.zacarias@gmail.com>
2023-09-28 20:27:10 +0200

    Updating ONOS data collector

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import requests
 import logging
-from time import sleep
+import os
+import time
+import influxdb_client
 from requests.auth import HTTPBasicAuth
+from urllib.error import URLError
 from onos import Link
-from pprint import pprint
 from rdflib import Namespace, XSD, Graph, Literal
 from rdflib.namespace import RDF, RDFS, OWL
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 from rdflib.graph import DATASET_DEFAULT_GRAPH_ID as default
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 ## Configuration
+# General 
+READ_INTERVAL = 5
+# ONOS
 ONOS_URL = "http://localhost:8181"
 ONOS_USER = "onos"
 ONOS_PASS = "rocks"
 ONOS_LINK_URL = f"{ONOS_URL}/onos/v1/links"
 ONOS_STAT_URL = f"{ONOS_URL}/onos/v1/statistics/flows/link"
 ONOS_PORT_STAT_URL = f"{ONOS_URL}/onos/v1/statistics/ports"
-READ_INTERVAL = 5
+# Apache Jena FUSEKI (SPARQL)
 FUSEKI_URL = "http://localhost:3030"
 FUSEKI_DATASET = "ONOS"
 FUSEKI_UPDATE_URL = f"{FUSEKI_URL}/{FUSEKI_DATASET}/update"
 FUSEKI_QUERY_URL = f"{FUSEKI_URL}/{FUSEKI_DATASET}/query"
+# InlfuxDB
+INFLUX_TOKEN = "h5qh4-C5BJxsnRrvw3wcCERvcBgLekZC63kDPECeg-FiundQzMvpApBbbRd2-hzNslVhCiOixhKPWMaL7hZbdw=="
+INFLUX_ORG = "influxdb"
+INFLUX_URL = "http://localhost:8086"
+INFLUX_BUCKET = "mininet"
+
+
 LINKS_DICTIONARY = {
     'path1': 
         {
@@ -41,36 +55,51 @@ LINKS_DICTIONARY = {
         },
 }
 
+def update_influx(link:Link, wirte_api):
+    point = (
+        Point("linkstats")
+        .tag(link.id, "linkusage")
+        .field("traffic", link.get_data_rate())
+    )
+    logging.info("Sending data to InfluxDB for link: %s", link.id)
+    wirte_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
+
 def create_ontology(graph: Graph, ns: Namespace):
     """
     Creates the RDF Structure and send ontology in the SPARQL Server
     """
-    graph.add((ns.DataRate, RDF.type, ns.Measurement))
-    graph.add((ns.DataRateBWMeasurement, RDF.type, ns.BWMeasurement))
-    graph.add((ns.hasBWMeasurement, RDF.type, OWL.ObjectProperty))
-    graph.add((ns.hasBWMeasurement, RDFS.domain, ns.Measurement))
-    graph.add((ns.hasBWMeasurement, RDFS.range, ns.BWMeasurement))
-    graph.add((ns.Usage5Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage10Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage15Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage20Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage25Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage30Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage35Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage40Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage45Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage50Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage55Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage60Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage65Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage70Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage75Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage80Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage85Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage90Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage95Percent, RDF.type, ns.BWMeasurement))
-    graph.add((ns.Usage100Percent, RDF.type, ns.BWMeasurement))
-    logging.info("Ontology sent to SPARQL Server. Graph has a lenght of %s", len(graph))
+    try:
+        graph.add((ns.LinkA, RDF.type, ns.Link))
+        graph.add((ns.LinkB, RDF.type, ns.Link))
+        graph.add((ns.DataRate, RDF.type, ns.Measurement))
+        graph.add((ns.DataRateBWMeasurement, RDF.type, ns.BWMeasurement))
+        graph.add((ns.hasBWMeasurement, RDF.type, OWL.ObjectProperty))
+        graph.add((ns.hasBWMeasurement, RDFS.domain, ns.Measurement))
+        graph.add((ns.hasBWMeasurement, RDFS.range, ns.BWMeasurement))
+        graph.add((ns.Usage5Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage10Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage15Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage20Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage25Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage30Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage35Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage40Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage45Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage50Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage55Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage60Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage65Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage70Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage75Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage80Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage85Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage90Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage95Percent, RDF.type, ns.BWMeasurement))
+        graph.add((ns.Usage100Percent, RDF.type, ns.BWMeasurement))
+        logging.info("Ontology sent to SPARQL Server. Graph has a lenght of %s", len(graph))
+    except URLError:
+        logging.error("Connection to SPARQL Server refused. Is SPARQL running?")
+
 
 
 def update_sparqldb(link: Link, graph: Graph, ns: Namespace):
@@ -122,13 +151,22 @@ def update_sparqldb(link: Link, graph: Graph, ns: Namespace):
         usageObject = ns.Usage100Percent
     else:
         logging.error('Error creating the link usage object')
-    # remove old values
-    graph.remove((ns.MyMeasurement, ns.hasBWMeasurement, None))
-    graph.remove((ns.MyMeasurement, RDF.type, ns.BWMeasurement))
-    # add new values 
-    graph.add((ns.MyMeasurement, RDF.type, ns.BWMeasurement))
-    graph.add((ns.MyMeasurement, ns.hasBWMeasurement, usageObject))
-    logging.info("Graph sent to SPARQL server with lenght %s", len(graph))
+    # Link to update
+    if link.id == "path1":
+        link_to_update = ns.LinkA
+    elif link.id == "path2":
+        link_to_update = ns.LinkB
+
+    try:
+        # remove old values
+        graph.remove((link_to_update, ns.hasBWMeasurement, None))
+        graph.remove((link_to_update, RDF.type, ns.BWMeasurement))
+        # add new values 
+        graph.add((link_to_update, RDF.type, ns.BWMeasurement))
+        graph.add((link_to_update, ns.hasBWMeasurement, usageObject))
+        logging.info("Updating BWMeasurement for link %s", link.id)
+    except URLError:
+        logging.error("Connection to SPARQL Server refused. Is SPARQL running?")
 
 def get_link_stats(link: Link) -> int:
     """ 
@@ -160,6 +198,11 @@ def main():
     ns = Namespace("http://6g-ric.de#")
     graph = Graph(store, identifier=default)
     create_ontology(graph, ns)
+    # Create Influx wirte client
+    influx_write_client = influxdb_client.InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
+    influx_write_api = influx_write_client.write_api(write_options=SYNCHRONOUS)
+
+    # Collect all linkst to monitor (static configuration)
     links = []
     for p in LINKS_DICTIONARY:
         links.append(Link(
@@ -174,19 +217,20 @@ def main():
         while True:
             # Get link stats
             for l in links:
+                logging.debug("Getting link statistics for link %s", l.id)
                 get_link_stats(l)
-            
-            # Get the link with highest utilization
-            max_usage = 0.0
-            link_to_update = None
-            for l in links:
-                if l.get_link_usage() > max_usage:
-                    max_usage = l.get_link_usage()
-                    link_to_update = l
-            update_sparqldb(link=link_to_update, graph=graph, ns=ns)
-            sleep(READ_INTERVAL)
+                logging.debug("Updating SPARQL database for link %s", l.id)
+                update_sparqldb(link=l, graph=graph, ns=ns)
+                logging.debug("Updating influxDB statistics for link %s", l.id)
+                update_influx(link=l, wirte_api=influx_write_api)
+            # Wait for the next iteration
+            time.sleep(READ_INTERVAL)
 
     except KeyboardInterrupt:
+        try:
+            graph.close()
+        except:
+            logging.info("SPARQL graph was not open. Impossible to close the connection.")
         print("Script finished by CTRL+C command")
 
 if __name__ == '__main__':
